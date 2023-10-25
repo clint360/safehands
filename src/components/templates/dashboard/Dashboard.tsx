f6c597b78d55630708a9f89291f791b597a3212d
Clint Animbom <clint360.rebase@gmail.com>
2023-10-25 13:37:56 +0100

    completed dashboard design

--- src/components/templates/dashboard/Dashboard.tsx
+++ src/components/templates/dashboard/Dashboard.tsx
@@ -9,16 +9,26 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
+  LineChart,
+  Line,
 } from "recharts";
 import "./Dashboard.scss";
 import Text from "@/components/atoms/Text";
 import CountUp from "react-countup";
 
 const Grid = styled.div`
+  margin-left: 1rem;
+  width: 100%;
   display: flex;
   flex-grow: 1;
   gap: 1rem;
   justify-content: space-between;
+
+  @media screen and (max-width: 736px) {
+    flex-direction: column;
+    align-items: center;
+    margin-left: 0;
+  }
 `;
 
 const StatCard = styled.div<{
@@ -38,6 +48,13 @@ const StatCard = styled.div<{
     border: 1px solid grey;
     scale: 1.015;
   }
+
+  @media screen and (max-width: 736px) {
+    width: 90%;
+    &:hover {
+      border: none;
+    }
+  }
 `;
 
 const Dashboard: React.FC = () => {
@@ -55,6 +72,19 @@ const Dashboard: React.FC = () => {
     { category: "Category D", reports: 5 },
     { category: "Category E", reports: 2 },
   ];
+
+  // Sample data for the line chart
+  const lineData = [
+    { month: "January", categoryA: 10, categoryB: 15 },
+    { month: "February", categoryA: 12, categoryB: 18 },
+    { month: "March", categoryA: 8, categoryB: 16 },
+    { month: "April", categoryA: 14, categoryB: 20 },
+    { month: "May", categoryA: 16, categoryB: 13 },
+    { month: "June", categoryA: 11, categoryB: 17 },
+  ];
+
+  // Generate an array of unique category names
+  const categories = Object.keys(lineData[0]).filter((key) => key !== "month");
 
   return (
     <div className="dashboard">
@@ -97,26 +127,41 @@ const Dashboard: React.FC = () => {
       <br />
       <br />
       <div className="chartContainer">
-        <h2>Reports by Category</h2>
-        <BarChart width={400} height={300} data={barChartData}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="category" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="reports" fill="#8884d8" />
-        </BarChart>
-      </div>
-      <div className="chartContainer">
-        <h2>Reports by Category</h2>
-        <BarChart width={400} height={300} data={barChartData}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="category" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="reports" fill="#8884d8" />
-        </BarChart>
+        <div className="chartdiv" style={{width: `100%`}}>
+          <BarChart
+            width={window.innerWidth > 736 ? (window.innerWidth/2 - 100) : (window.innerWidth-50)}
+            height={300}
+            data={barChartData}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="category" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="reports" fill="#333333" />
+          </BarChart>
+        </div>
+        <div className="chartdiv">
+          <LineChart
+            width={window.innerWidth > 736 ? (window.innerWidth/2 - 100) : (window.innerWidth-50)}
+            height={300}
+            data={lineData}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            {categories.map((category, index) => (
+              <Line
+                key={index}
+                type="monotone"
+                dataKey={category}
+                stroke={`#${Math.floor(Math.random() * 16777215).toString(16)}`}
+              />
+            ))}
+          </LineChart>
+        </div>
       </div>
     </div>
   );
