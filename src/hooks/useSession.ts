905bf7f3f719c1c469b1fa6d3adf3c132da1241d
Clint Animbom <clint360.rebase@gmail.com>
2023-11-01 12:56:35 +0100

    delete unecessary items

--- src/hooks/useSession.ts
+++
@@ -1,8 +0,0 @@
-import { UserContext } from "@/context/UserContext";
-import { useContext } from "react";
-
-// hook that can be used to get the session data
-export function useSession() {
-    const context = useContext(UserContext);
-    return context;
-  };
