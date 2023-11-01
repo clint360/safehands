1038b5471a42a2c90fb36dab76a070e863695bc2
Clint Animbom <clint360.rebase@gmail.com>
2023-11-01 11:05:22 +0100

    EDITED MJS

--- src/services/messages.ts
+++ src/services/messages.ts
@@ -1,9 +1,5 @@
 import { Message } from "@/domains/Messages";
 
-export async function getMessagesById(userId: string) {
-    
-}
-
 export async function getUnseenMessagesCountForId(userId: string) {
     return 0
 }
